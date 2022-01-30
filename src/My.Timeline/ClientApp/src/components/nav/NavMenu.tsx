e54984563fc2c6faabcec5f0b3cef2135da7054c
Vincent Shen <SHENVIN@chrobinson.com>
2022-01-30 10:45:24 +0800

    navbar

---
+++ src/My.Timeline/ClientApp/src/components/nav/NavMenu.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export class NavMenu extends React.PureComponent<{}> {
+  public render(): React.ReactNode {
+      return (
+        <nav className="navbar is-fixed-top is-transparent is-primary top-nav" role="navigation" aria-label="main navigation">
+          <div className="container">
+            <div className="navbar-brand">
+              <a className="navbar-item" >
+                <img src="../assets/favicon.png"></img>
+              </a>
+              <a className="navbar-item">
+                <img src="../assets/logo.png"></img>
+              </a>
+            </div>
+          </div>
+        </nav>
+      );
+  }
+}
