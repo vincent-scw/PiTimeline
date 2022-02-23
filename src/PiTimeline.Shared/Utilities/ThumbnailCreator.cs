677078d8dd37da0f384223f05829ac770ec407c7
Vincent Shen <SHENVIN@chrobinson.com>
2022-02-23 15:17:33 +0800

    thumbnail creator

--- src/PiTimeline.Shared/Utilities/ThumbnailCreator.cs
+++ src/PiTimeline.Shared/Utilities/ThumbnailCreator.cs
@@ -4,10 +4,13 @@ namespace PiTimeline.Shared.Utilities
 {
     public static class ThumbnailCreator
     {
-        public static string CreateThumbnail(string imgPath, int newHeight, int newWidth)
+        private const int MaxHeight = 720;
+
+        public static void CreateThumbnail(string imgPath, string outputPath)
         {
-            var resizeFactor = 0.5f;
             using var bitmap = SKBitmap.Decode(imgPath);
+
+            var resizeFactor = bitmap.Height > MaxHeight ? (float)MaxHeight / bitmap.Height : 1f;
 
             using var toBitmap = new SKBitmap((int)Math.Round(bitmap.Width * resizeFactor), (int)Math.Round(bitmap.Height * resizeFactor), bitmap.ColorType, bitmap.AlphaType);
 
@@ -17,25 +20,13 @@ namespace PiTimeline.Shared.Utilities
             canvas.DrawBitmap(bitmap, 0, 0);
             canvas.ResetMatrix();
 
-            using var font = SKTypeface.FromFamilyName("Arial");
-            using var brush = new SKPaint
-            {
-                Typeface = font,
-                TextSize = 64.0f,
-                IsAntialias = true,
-                Color = new SKColor(255, 255, 255, 255)
-            };
-            canvas.DrawText("Resized!", 0, bitmap.Height * resizeFactor / 2.0f, brush);
-
             canvas.Flush();
 
             using var image = SKImage.FromBitmap(toBitmap);
             using var data = image.Encode(SKEncodedImageFormat.Jpeg, 90);
 
-            using (var stream = new FileStream("output.jpg", FileMode.Create, FileAccess.Write))
-                data.SaveTo(stream);
-
-            return "";
+            using var stream = new FileStream(outputPath, FileMode.Create, FileAccess.Write);
+            data.SaveTo(stream);
         }
     }
 }
--- test/PiTimeline.UnitTests/Shared/ThumbnailCreatorTests.cs
+++ test/PiTimeline.UnitTests/Shared/ThumbnailCreatorTests.cs
@@ -1,3 +1,5 @@
+using System.IO;
+using PiTimeline.Shared.Utilities;
 using Xunit;
 
 namespace PiTimeline.UnitTests.Shared
@@ -5,9 +7,13 @@ namespace PiTimeline.UnitTests.Shared
     public class ThumbnailCreatorTests
     {
         [Fact]
-        public void Test1()
+        public void CreateThumbnail_ShouldSucceed()
         {
+            var input = "magnificent-picture.jpg";
+            var output = "magnificent-picture-thumbnail.jpg";
+            ThumbnailCreator.CreateThumbnail(input, output);
 
+            Assert.True(File.Exists(output));
         }
     }
 }
