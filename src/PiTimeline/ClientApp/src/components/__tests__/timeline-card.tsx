9ec730fe629b8568d80603821a54a4202a93e6e3
Vincent Shen <SHENVIN@chrobinson.com>
2022-05-25 17:00:55 +0800

    fix date issue

--- src/PiTimeline/ClientApp/src/components/__tests__/timeline-card.tsx
+++ src/PiTimeline/ClientApp/src/components/__tests__/timeline-card.tsx
@@ -10,7 +10,7 @@ it('renders correctly', () => {
     title: 'timeline',
     description: 'desc',
     coverPatternUrl: 'http://mock',
-    since: new Date(2022, 5, 1, 0, 0, 0, 0) // 01 Jun 2022, month is zero-based
+    since: new Date('2022-06-01T00:00:00.000Z') // 01 Jun 2022
   };
   const delFunc = jest.fn();
   const wrapper = mount(<MemoryRouter><TimelineCard data={timeline} deleteTimeline={delFunc} /></MemoryRouter>);
