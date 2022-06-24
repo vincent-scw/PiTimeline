bc287b19874c9628a17a67a4bf10549d2766f0be
Vincent Shen <edentidus@foxmail.com>
2022-06-24 13:03:34 +0800

    add migrations

---
+++ src/PiTimeline.Infrastructure/Repo/MyDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace PiTimeline.Infrastructure.Repo
+{
+    public class MyDbContextFactory : IDesignTimeDbContextFactory<MyDbContext>
+    {
+        public MyDbContext CreateDbContext(string[] args)
+        {
+           var optionsBuilder = new DbContextOptionsBuilder<MyDbContext>();
+            optionsBuilder.UseSqlite("Data Source=timeline.db");
+
+            return new MyDbContext(optionsBuilder.Options);
+        }
+    }
+}
--- src/PiTimeline/Startup.cs
+++ src/PiTimeline/Startup.cs
@@ -81,7 +81,7 @@ namespace PiTimeline
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MyDbContext dbContext)
         {
             if (env.IsDevelopment())
             {
@@ -94,6 +94,8 @@ namespace PiTimeline
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            dbContext.Database.Migrate();
 
             //app.UseHttpsRedirection();
             app.UseStaticFiles();
